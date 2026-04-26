246239a68d6101c2333f9491de5e61b08f5827d9
Rajdeep Rudra <rajdeeprudra2003@gmail.com>
2026-04-26 13:32:33 +0530

    Prompts to send the llms are added , and route logic to hit the llm added

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import {tavily} from '@tavily/core';
-
+import { Output, streamText } from 'ai';
+import z from 'zod';
+import { PROMPT_TEMPLATE, SYSTEM_PROMPT } from './prompt';
 const client = tavily({ apiKey: process.env.TAVILY_API_KEY });
 
 
@@ -17,7 +19,7 @@ app.post("/Perflexity_ask", async(req,res)=>{
 
     //step 3(TODO)- check is we have web search indexed for a similar query
 
-    //              web search to gather resources
+    // step-4             web search to gather resources
 
        const webSearchResponse = await client.search("", {
         searchDepth: "advanced"
@@ -26,12 +28,26 @@ app.post("/Perflexity_ask", async(req,res)=>{
         const webSearchResult = webSearchResponse.results;
         
 
-    //step 4- do some context engineering on the pormpt + web search responses
+    //step 5- do some context engineering on the pormpt + web search responses
 
-    //step 5- hit the LLM and stream back response
+    //step 6- hit the LLM and stream back response(vercel ai gateway)
 
+    const prompt = PROMPT_TEMPLATE
+        .replace("{{WEB_SEARCH_RESULTS}}",JSON.stringify(webSearchResult) )
+        .replace("{{USER_QUERY}}",query)
+   
+    const result = streamText({
+        model: 'openai/gpt-5.4',
+        prompt: 'Invent a new holiday and describe its traditions.',
+        system: SYSTEM_PROMPT,
+        
+    });
+
+    for await (const textPart of result.textStream){
+       res.end(textPart);
+    }
     //step 6- also stream beack the sources and the follow up questions (which we can get from another parallel LLM call)
-})
+});
     
 
 
---
+++ backend/prompt.ts
@@ -0,0 +1,41 @@
+export const SYSTEM_PROMPT = `
+    You are an expert assistant called Perflexity. Your job is simple, given the USER_QUERY and
+    a bunch of web search responses, try to answer the user query to the best of your abilities.
+    YOU DONT HAVE ACCESS TO ANY TOOLS. You are being given all the context that is needed
+    to answer the query.
+
+    You also need to return follow up questions to the user based on the questions they have asked.
+    The response needs to be structured like this - 
+    <ANSWER>
+    This is where the actual query should be answered
+    </ANSWER>
+
+    <FOLLOW_UPS>
+        <question>first follow up question</question>
+        <question>second follow up question</question>
+        <question>third follow up question</question>
+    </FOLLOW_UPS>
+
+    Example - 
+    Query - I want to learn rust, can you suggest best ways to do it
+    Response - 
+
+    <ANSWER>
+    For sure, the best resource to learn rust is the rust book
+    </ANSWER>
+
+    <FOLLOW_UPS>
+        <question> How can i learn advance rust </question>
+        <question> How is rust better than typescript </question>
+    </FOLLOW_UPS>
+
+
+`
+
+export const PROMPT_TEMPLATE = `
+    ## Web search results
+    {{WEB_SEARCH_RESULTS}}
+
+    ## USER_QUERY
+    {{USER_QUERY}}
+`
